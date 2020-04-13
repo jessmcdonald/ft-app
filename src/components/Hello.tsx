3f3a773337e3cb33be084c34ede034fd5e8001f0
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-04-13 14:41:33 +0200

    webpack config

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,1 +1,17 @@
 import * as React from "react";
+
+export interface HelloProps {
+  compiler: string;
+  framework: string;
+}
+
+//state is never set to type {} used
+export class Hello extends React.Component<HelloProps, {}> {
+  render() {
+    return (
+      <h1>
+        FT app built with {this.props.compiler} and {this.props.framework}!
+      </h1>
+    );
+  }
+}
